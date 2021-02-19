12c2e477c125f05e0fa02414cdcbb6eb434daad2
PETCU Barbu CONSULTANT SOFTGALAXY <Barbu.Petcu@orange.com>
2021-02-19 17:09:54 +0200

    first commit

---
+++ src/test/java/com/test/current/account/CurrentAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.current.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrentAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
